05b344a67e8f72bde28f54d9bb4ed74a74b4e052
SUJAIT ULLAH <programming29666@gmail.com>
2024-11-08 23:35:35 +0600

    addedEnd

--- problem-8.ts
+++ problem-8.ts
@@ -1,5 +1,6 @@
 {
     //////////////////////////////////
+    //Problem-8
     // Create a Function ValidateKey(Obj:Text,Key:(Keyof T)[]) that takes an object obj and an array of keys MediaKeySession.The Function should return true if all of the specified keys exist in the objects , otherwise, it should return false.
     
     const Obj = { name: "Sujait",age:23};
@@ -17,7 +18,7 @@
     
       console.log(validateKey(Obj, ["name","age"]));
       console.log(validateKey(Obj, ["age"])); 
-      console.log(validateKey(Obj, ["ID"])); 
+    //   console.log(validateKey(Obj, ["ID"])); 
     
     ///////////////////////////////////
     }
